35ca1dd393af98221e12667d53650dc4cc5f1fa1
tsk <tsk@thesimplekid.com>
2026-09-03 08:26:18 +0000

    chore: update flake

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Fiach";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
 
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
